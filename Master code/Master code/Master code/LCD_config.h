98410da60c1b869a8b8ae7d0296d7765178d721c
GeorgeElhabib <111635409+GeorgeElhabib@users.noreply.github.com>
2023-02-19 15:23:36 +0200

    Add files via upload
    Master Code

---
+++ Master code/Master code/Master code/LCD_config.h
@@ -0,0 +1,15 @@
+/*
+ * LCD_config.h
+ *
+* Created: 2/18/2023 8:43:57 AM
+*  Author: Ismail
+ */ 
+
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+
+#define eight_bits_mode
+//#define four_bits_mode
+
+#endif /* LCD_CONFIG_H_ */
