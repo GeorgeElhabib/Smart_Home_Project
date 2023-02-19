79938cead3e091dbb817813786fce84def7fc587
GeorgeElhabib <111635409+GeorgeElhabib@users.noreply.github.com>
2023-02-19 15:24:46 +0200

    Add files via upload
    Slave Code

---
+++ Slave code/Slave code/Slave code/DIO_Slave_cfg.h
@@ -0,0 +1,27 @@
+/*
+ * DIO_Slave_cfg.h
+ *
+* Created: 2/18/2023 8:43:57 AM
+*  Author: Ismail
+ */ 
+
+
+#ifndef DIO_SLAVE_CFG_H_
+#define DIO_SLAVE_CFG_H_
+#define ROOM1_LED_PORT			  'D'
+#define ROOM2_LED_PORT			  'D'
+#define ROOM3_LED_PORT			  'D'
+#define ROOM4_LED_PORT			  'D'
+#define ROOM5_LED_PORT			  'D'
+#define TV_LED_PORT				  'D'
+#define AIR_CONDITIONING_LED_PORT 'D'
+/***************************************/
+#define ROOM1_LED_PIN			   1
+#define ROOM2_LED_PIN			   2
+#define ROOM3_LED_PIN			   3
+#define ROOM4_LED_PIN			   4
+#define ROOM5_LED_PIN			   5
+#define TV_LED_PIN				   6
+#define AIR_CONDITIONING_LED_PIN   7
+
+#endif /* DIO_SLAVE_CFG_H_ */
